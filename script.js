c96c2014286525829d3e1ec268ec78ad1c5e1032
unknown <Minskoleopc>
2024-05-02 21:24:31 +0530

    added

---
+++ script.js
@@ -0,0 +1,45 @@
+
+// program 1
+// what are variables ??
+// let and const , var
+
+let x = 10
+console.log(x)
+x = 100
+console.log(x)
+
+const j = 100
+console.log(j)
+//j = 999
+
+// Arithmetic operation 
+// addition , subtraction , multiplication , division , modulus
+//   +        ,    -        ,    *           ,   /      ,   %
+
+let a = 10
+let b = 5
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+let e = 100
+let f = 50
+
+
+console.log(e+f)
+console.log(e-f)
+console.log(e*f)
+console.log(e/f)
+console.log(e%f)
+
+
+
+
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,66 @@
+
+// variables 
+// let and const 
+
+let a = 10
+console.log(a)
+a = 100
+console.log(a)
+
+
+const j = 100
+console.log(j)
+//j = 200
+
+
+// program 2
+
+let s = 10
+let t = 5
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+
+let q = 8
+let r = 4
+
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+// function - keyword
+// Calculator - functionName
+// () - parenthesis
+// x,y - parameter
+// {} - blocks
+// Calculator()
+// 4,2 - arguments
+
+
+// 10 pair ----- 50 lines
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(18,9)
+Calculator(4,2)
+
+
+
+
+
+
+
+
+
+
+
