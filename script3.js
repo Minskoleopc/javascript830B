174821bd39fc6becb93ef2f0af58958210e26cef
unknown <Minskoleopc>
2024-05-06 20:52:00 +0530

    added

---
+++ script3.js
@@ -0,0 +1,82 @@
+// // let and const
+
+// let a = 10 
+// console.log(a)
+// a  = 300
+// console.log(a)
+
+
+// const h = 100 
+// console.log(h)
+// //h = 1000
+
+// // program 2
+
+// let s  = 10
+// let t = 5
+
+// console.log(s+t)
+// console.log(s-t)
+// console.log(s*t)
+// console.log(s/t)
+// console.log(s%t)
+
+// let a1 = 8 
+// let b1 = 4
+
+// console.log(a1 + b1)
+// console.log(a1 - b1)
+// console.log(a1 * b1)
+// console.log(a1 / b1)
+// console.log(a1 % b1)
+
+// // 10 pair ----- 50 lines
+
+// function Calculator(x,y){
+//     console.log(x+y)
+//     console.log(x-y)
+//     console.log(x*y)
+//     console.log(x/y)
+//     console.log(x%y)
+// }
+// Calculator(12,4)
+// Calculator(8,4)
+// Calculator(4,2)
+
+
+// Day 2
+
+// function without parameter and without return type 
+
+function add(){
+    console.log(9+9)
+}
+add()
+add()
+add()
+add()
+add()
+
+// function with parameter and without return type 
+
+function addB(x,y){
+    console.log(x+y)
+}
+addB(124,4)
+addB(12,4)
+
+// function with parameter and function with return type
+
+function addC(x,y){
+    return x + y
+}
+
+let a = addC(12,4)
+console.log(a)
+console.log(a+a)
+
+
+
+
+
+
---
+++ script4.js
@@ -0,0 +1,31 @@
+// introvert     and    extrovert 
+// calm                  loud
+// less social           more social
+// less outing           more outing
+
+// human 
+// Properties - hand , color , height , weight , age 
+// Methods  - walk() , talk()
+
+// vehicle 
+// Properties - color , model , type , companyName
+// Methods - start() , stop()
+
+// Bank acc
+// Properties - accNo , bal , IFSC code, accName
+// Methods - deposit() , withdrawl()
+
+let a  = 10
+console.log(a)
+console.log(typeof a)
+// 10 , 10.5 , -10.5 , 10.89 , 00.00
+
+
+let canDrive  = true
+console.log(canDrive)
+console.log(typeof canDrive)
+//frue or false
+
+let firstName = "chinmay"
+console.log(firstName)
+console.log(typeof firstName)
