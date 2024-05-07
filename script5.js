392dd1dfa145924c9a66f050cd7f9796af171cdf
unknown <Minskoleopc>
2024-05-07 21:01:01 +0530

    added

---
+++ script5.js
@@ -0,0 +1,96 @@
+// introver extrovert
+
+
+// Human - type 
+// properties - color weight height 
+// methods - walk() , talk()
+
+
+// Vehicle 
+// properties - regNo , color , model , type
+// method - start() , stop()
+
+// bank acc 
+// properties - accNO , accName , bal
+// methods - deposit() , withdrawl()
+
+
+let a = 10
+console.log(a)
+console.log(typeof a)
+// 10 , -10 ,10.5 , -12.5
+
+
+let b = true
+console.log(b)
+console.log(typeof b)
+// true or false
+
+
+let c = "chinmay"
+console.log(c)
+console.log(typeof c)
+//string
+// "A", "a", '123', "a123#@$"," "
+
+// comparison operator 
+
+console.log(typeof 10)
+console.log(typeof '10')
+
+// entity < entity ----> boolean -----> true or false
+// < , > ,<= ,>=
+// == , !=  // value
+// === , !== // value and type
+
+console.log(10 === '10')
+console.log(10 === 10)
+console.log(10 == '10')
+console.log(10 !== '10')
+console.log(10 == 10)
+console.log(4 > 3)
+console.log(5 < 3)
+console.log(2 >= 3)
+console.log(3 >= 2)
+console.log(2 >= 2)
+console.log(2 <= 2)
+
+
+// logical operator 
+
+
+// AND - &&
+
+// true   &&   true  ----- true
+// false  &&   true  ----- false
+// true   &&   false  ----- false
+// false  &&   false  ----- false
+
+console.log(2 == '2' && 3 === 3)
+console.log(3 != 3 && 6 == 6)
+console.log(7 == 7 && 3 === '3')
+console.log(7 == 6 && 3 === '3')
+
+// OR - ||
+// true   ||   true  ----- true
+// false  ||   true  ----- true
+// true   ||   false  ----- true
+// false  ||   false  ----- false
+
+console.log(2 == '2' || 3 === 3)
+console.log(3 != 3 || 6 == 6)
+console.log(7 == 7 || 3 === '3')
+console.log(7 == 6 || 3 === '3')
+
+// Not - !
+
+//! true  ===> false
+// !false ===> true
+
+console.log(!(8 === 8))
+console.log(! (8 === 7))
+
+
+
+
+// logical operator
