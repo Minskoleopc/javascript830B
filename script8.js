011b0dfbfeb834e5191d60ff2d2a6a69a403a6be
unknown <Minskoleopc>
2024-05-10 20:49:45 +0530

    added

---
+++ script8.js
@@ -0,0 +1,70 @@
+// for and while 
+
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+
+// for loop
+// for(intialization; conditionCheck ; increment/decrement){
+//     // statatements
+// }
+
+// program 1 print 1 to 5
+
+for (let i = 1; i <= 5; i++) {  // 2 // 3 // 4 //5 // 6
+    console.log(i)  // 1 // 2 // 3 // 4 // 5
+}
+
+// 1 <= 5 --- true
+// 2 <= 5 --- true 
+// 3 <= 5 --- true
+// 4 <= 5 --- true
+// 5 <= 5 --- true
+
+// program 2 print "hello" 3 tyms
+
+for (let i = 1; i <= 3; i++) { // 2 // 3 // 4
+    console.log("hello") // "hello" , "hello" , "hello"
+}
+
+
+// program 3 print 1 to 1
+for (let i = 1; i <= 5; i++) { // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 ,2 , 3 , 4 , 5
+}
+
+// program 4 print 5 to 1
+for (let i = 5; i >= 1; i--) { // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3 // 2 //1
+}
+
+// program 5
+// print table of two
+
+for (let i = 2; i <= 20; i = i + 2) { // 4 // 6 // 8 // 10 --- 22
+    console.log(i) // 2 // 4 // 6 // 8 ---------- 20
+}
+
+for(let i = 1 ; i <= 10 ; i++){
+    console.log(i*2)
+}
+
+// program 6
+
+// table of 5 in reverse
+for(let i = 50 ; i >= 5 ; i= i-5){
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
