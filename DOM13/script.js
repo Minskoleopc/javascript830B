357f51c854010c5b86d550ab0b9fd5d765bea656
unknown <Minskoleopc>
2024-07-08 20:56:08 +0530

    added

---
+++ DOM13/script.js
@@ -0,0 +1,9 @@
+
+
+// let para = document.querySelector('p')
+// let buttonA = document.querySelector('button')
+
+// buttonA.addEventListener('click',function(){
+//     para.removeAttribute('class')
+//     para.classList.add("three")
+// })
---
+++ DOM14/script.js
@@ -0,0 +1,55 @@
+
+let addLi = document.querySelectorAll('li')
+let buttonA = document.querySelector('button')
+let ulList = document.querySelector('ul')
+
+// click 
+// buttonA.addEventListener('click',function(){
+//     for(let i = 0 ; i < addLi.length ; i++){
+//         // console.log(i)
+//         // console.log(addLi[i])
+//         addLi[i].style.color = "red"
+//     }
+// })
+
+//dblclick
+// buttonA.addEventListener('dblclick',function(){
+//     for(let i = 0 ; i < addLi.length ; i++){
+//         // console.log(i)
+//         // console.log(addLi[i])
+//         addLi[i].style.color = "red"
+//     }
+// })
+
+
+// mouseover 
+
+// buttonA.addEventListener('mouseover',function(){
+//         for(let i = 0 ; i < addLi.length ; i++){
+//             // console.log(i)
+//             // console.log(addLi[i])
+//             addLi[i].style.color = "red"
+//         }
+// })
+
+// ulList.addEventListener('mouseover',function(){
+//         for(let i = 0 ; i < addLi.length ; i++){
+//             addLi[i].textContent =  addLi[i].textContent.toUpperCase()
+//         }
+// })
+// ulList.addEventListener('mouseout',function(){
+//     for(let i = 0 ; i < addLi.length ; i++){
+//         addLi[i].textContent =  addLi[i].textContent.toLowerCase()
+//     }
+// })
+
+// event bubbling concept
+
+// reload
+buttonA.addEventListener('click',function(){
+    location.reload()
+})
+
+// Devops  ----- git / github // postman nueman // docker - 33 %
+
+// Cloudcomputing 
